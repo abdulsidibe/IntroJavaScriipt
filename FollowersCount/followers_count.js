d6a30f81d24e8244fbd1a7069f420a7a02961c6e
abdulsidibe <abdulsidibe@yahoo.ca>
2026-05-03 02:22:15 -0600

    added a set fuction to reset followers to zero

--- FollowersCount/followers_count.js
+++ FollowersCount/followers_count.js
@@ -1,13 +1,16 @@
 let count = 0; // Initialize count to 0
 
 function increaseCount() {
-  count++; // Increment the count by 1
+  count++;
+
   displayCount(); // Display the count
   checkCountValue(); //checck count value and display message
+  //resetCount();
 }
 
 function displayCount() {
-document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+  document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+  //document.getElementById("resetDisplay").innerHTML = count;
 }
 function checkCountValue() {
   if (count === 10) {
@@ -16,3 +19,9 @@ function checkCountValue() {
     alert("Your Instagram post gained 20 followers! Keep it up!");
   }
 }
+function resetCount() {
+    count = 0; // Reset the internal value
+    alert("Click ok to reset your followers to zero")
+    displayCount();
+}
+
